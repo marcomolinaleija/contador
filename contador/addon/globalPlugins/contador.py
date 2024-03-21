6b6c5a27cc1c5f635f5e58bd58baed1d9ffd7347
Marco <141619450+marcomolinaleija@users.noreply.github.com>
2024-03-20 22:04:44 -0600

    Update contador.py

--- contador/addon/globalPlugins/contador.py
+++ contador/addon/globalPlugins/contador.py
@@ -19,6 +19,7 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
             ui.message(message)
         else:
             ui.message("No hay texto seleccionado.")
+
     @scriptHandler.script(
         description="Cuenta del portapapeles la cantidad de palabras de un texto.",
         gesture="kb:NVDA+shift+W",
@@ -34,7 +35,21 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
             ui.message(message)
         else:
             ui.message("No hay texto seleccionado.")
+
+    @scriptHandler.script(
+        description="Toma el texto de portapapeles, mostrándolo en una ventana de mensaje de NVDA.",
+        gesture="kb:NVDA+ctrl+E",
+        category="Contador"
+    )
+    def script_readText(self, gesture):
+        text = api.getClipData()
+        if text:
+            ui.browseableMessage(text, "Texto", isHtml=False)
+        else:
+            ui.message("Nó hay texto en el portapapeles")
+
     def __init__(self):
         super(GlobalPlugin, self).__init__()
         self.bindGesture("kb:NVDA+shift+c", "countCharacters")
         self.bindGesture("kb:NVDA+shift+w", "countWords")
+        self.bindGesture("kb:NVDA+ctrl+E", "readText")
